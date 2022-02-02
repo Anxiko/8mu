0378f9534ab6026f70e5304d5fdf9066e82add96
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2022-02-03 00:26:35 +0100

    Add beeping sound test to main

--- main.c
+++ main.c
@@ -10,10 +10,15 @@
 #include "emulator.h"
 
 #include "SDL.h"
+#include "SDL_mixer.h"
+
+#define MIXER_CHUNK_SIZE 1024
+#define MIXER_CHANNELS_REQUESTED 1
+
 
 uint8_t rom[ROM_SIZE];
 
-void handle_sdl_error(bool error, const char *error_msg);
+void quit_on_sdl_error(bool error, const char *error_msg);
 
 int main(int argc, const char *argv[]) {
 	if (argc != 2) {
@@ -24,16 +29,32 @@ int main(int argc, const char *argv[]) {
 	const char *rom_path = argv[1];
 
 	SDL_SetMainReady();
-	handle_sdl_error(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0, "Failed to initialize SDL");
+	quit_on_sdl_error(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0, "Failed to initialize SDL");
+
+	quit_on_sdl_error(Mix_OpenAudio(
+		MIX_DEFAULT_FREQUENCY,
+		MIX_DEFAULT_FORMAT,
+		MIX_DEFAULT_CHANNELS,
+		MIXER_CHUNK_SIZE
+	) < 0, "Could not open audio");
+	quit_on_sdl_error(
+		Mix_AllocateChannels(MIXER_CHANNELS_REQUESTED) < 0,
+		"Failed to allocate channels"
+	);
+	Mix_Chunk *mix_chunk = Mix_LoadWAV("data/beep.wav");
+	if (mix_chunk == NULL) {
+		fprintf(stderr, "Failed to load sound effect");
+		exit(EXIT_FAILURE);
+	}
 
 	SDL_Window *window = SDL_CreateWindow(
 		"Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 		SCREEN_WIDTH * PIXEL_SCALING, SCREEN_HEIGHT * PIXEL_SCALING, 0
 	);
-	handle_sdl_error(window == NULL, "Failed to create window: %s");
+	quit_on_sdl_error(window == NULL, "Failed to create window: %s");
 
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	handle_sdl_error(renderer == NULL, "Failed to create renderer");
+	quit_on_sdl_error(renderer == NULL, "Failed to create renderer");
 
 	memset(rom, 0, ROM_SIZE);
 	FILE *file_ptr = fopen(rom_path, "rb");
@@ -74,9 +95,19 @@ int main(int argc, const char *argv[]) {
 						}
 					}
 				}
+				case SDL_KEYDOWN: {
+					switch (e.key.keysym.sym) {
+						case SDLK_SPACE: {
+							Mix_PlayChannel(-1, mix_chunk, 0);
+						}
+					}
+				}
 			}
 		}
 	}
+
+	Mix_FreeChunk(mix_chunk);
+	Mix_CloseAudio();
 
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
@@ -85,7 +116,7 @@ int main(int argc, const char *argv[]) {
 	return EXIT_SUCCESS;
 }
 
-void handle_sdl_error(bool error, const char *error_msg) {
+void quit_on_sdl_error(bool error, const char *error_msg) {
 	if (error) {
 		printf("%s: %s", error_msg, SDL_GetError());
 		exit(EXIT_FAILURE);
