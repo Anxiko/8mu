f13267e2510e3ff1630586daf399f4982d60b72f
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2022-02-17 00:27:21 +0100

    Add Unity testing, add instructions test file

---
+++ tests/instructions.c
@@ -0,0 +1,21 @@
+#include <unity.h>
+
+void setUp(){
+
+}
+
+void tearDown() {
+
+}
+
+void test_testing() {
+	TEST_ASSERT(1);
+}
+
+int main() {
+	UNITY_BEGIN();
+
+	RUN_TEST(test_testing);
+
+	return UNITY_END();
+}
