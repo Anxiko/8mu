0e63cb951518fe1571ccb8028e217f69e70d0a32
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2022-02-19 23:16:18 +0100

    Add tests for memory reading

--- src/tests/core.c
+++ src/tests/core.c
@@ -3,6 +3,7 @@
 #include "state.h"
 #include "beeper.h"
 #include "keyboard.h"
+#include "memory.h"
 
 CpuState cpu_state;
 
@@ -40,20 +41,32 @@ void test_keyboard_is_key_pressed() {
 void test_keyboard_any_key_pressed() {
 	uint8_t pressed_key = 127; // Non-valid key, to check if the value has been altered at all
 	TEST_ASSERT_FALSE(any_key_pressed(&cpu_state, &pressed_key));
-	TEST_ASSERT_EQUAL_UINT8(pressed_key, 127);
+	TEST_ASSERT_EQUAL_UINT8(127, pressed_key);
 
 	cpu_state.keyboard[4] = true;
 	cpu_state.keyboard[6] = true;
 	TEST_ASSERT_TRUE(any_key_pressed(&cpu_state, &pressed_key));
-	TEST_ASSERT_EQUAL_UINT8(pressed_key, 4);
+	TEST_ASSERT_EQUAL_UINT8(4, pressed_key);
+}
+
+
+void test_memory_read() {
+	uint8_t rom[ROM_SIZE] = {0x12, 0x34};
+	init_state(&cpu_state, rom);
+
+	TEST_ASSERT_EQUAL_UINT8(0x12, read_byte_memory(&cpu_state, ROM_ADDRESS_START));
+	TEST_ASSERT_EQUAL_UINT16(0x1234, read_word_memory(&cpu_state, ROM_ADDRESS_START));
 }
 
 int main() {
 	UNITY_BEGIN();
 
 	RUN_TEST(test_beeper_set_beeper_state);
+
 	RUN_TEST(test_keyboard_is_key_pressed);
 	RUN_TEST(test_keyboard_any_key_pressed);
+
+	RUN_TEST(test_memory_read);
 
 	return UNITY_END();
 }
