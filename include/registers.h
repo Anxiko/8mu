8df3eb78b95fbb9dbc5f07616c0b8e85d86a1db8
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2022-02-19 16:20:29 +0100

    Adapt registers to work with cpu state

--- include/registers.h
+++ include/registers.h
@@ -2,20 +2,22 @@
 #define CHIP8_REGISTERS_H
 
 #include <stdint.h>
+
+#include "state.h"
 #include "memory.h"
 
-void initialize_registers();
+void initialize_registers(CpuState *cpu_state);
 
-uint16_t read_register_pc();
+uint16_t read_register_pc(CpuState *cpu_state);
 
-void write_register_pc(uint16_t v);
+void write_register_pc(CpuState *cpu_state, uint16_t v);
 
-uint16_t read_index_register();
+uint16_t read_index_register(CpuState *cpu_state);
 
-void write_index_register(uint16_t v);
+void write_index_register(CpuState *cpu_state, uint16_t v);
 
-uint8_t read_register_bank(uint8_t r);
+uint8_t read_register_bank(CpuState *cpu_state, uint8_t r);
 
-void write_register_bank(uint8_t r, uint8_t v);
+void write_register_bank(CpuState *cpu_state, uint8_t r, uint8_t v);
 
 #endif //CHIP8_REGISTERS_H
--- src/registers.c
+++ src/registers.c
@@ -1,33 +1,29 @@
 #include "registers.h"
 
-uint16_t program_counter;
-uint16_t index_register;
-uint8_t bank[REGISTERS];
-
-void initialize_registers() {
-	write_register_pc(ROM_ADDRESS_START);
+void initialize_registers(CpuState *cpu_state) {
+	write_register_pc(cpu_state, ROM_ADDRESS_START);
 }
 
-uint16_t read_register_pc() {
-	return program_counter;
+uint16_t read_register_pc(CpuState *cpu_state) {
+	return cpu_state->program_counter;
 }
 
-void write_register_pc(uint16_t v) {
-	program_counter = v;
+void write_register_pc(CpuState *cpu_state, uint16_t v) {
+	cpu_state->program_counter = v;
 }
 
-uint16_t read_index_register() {
-	return index_register;
+uint16_t read_index_register(CpuState *cpu_state) {
+	return cpu_state->index_register;
 }
 
-void write_index_register(uint16_t v) {
-	index_register = v;
+void write_index_register(CpuState *cpu_state, uint16_t v) {
+	cpu_state->index_register = v;
 }
 
-uint8_t read_register_bank(uint8_t r) {
-	return bank[r];
+uint8_t read_register_bank(CpuState *cpu_state, uint8_t r) {
+	return cpu_state->register_bank[r];
 }
 
-void write_register_bank(uint8_t r, uint8_t v) {
-	bank[r] = v;
+void write_register_bank(CpuState *cpu_state, uint8_t r, uint8_t v) {
+	cpu_state->register_bank[r] = v;
 }
