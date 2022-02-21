38b9aec25e942492d45b711ab8d4f8d5b0c00c46
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2022-02-22 00:21:15 +0100

    Test clear screen

--- src/tests/instructions.c
+++ src/tests/instructions.c
@@ -1,21 +1,37 @@
 #include "unity.h"
 
-void setUp(){
+#include "state.h"
+#include "screen.h"
+#include "instructions.h"
 
+CpuState cpu_state;
+
+void setUp() {
+	init_state(&cpu_state, NULL);
 }
 
 void tearDown() {
 
 }
 
-void test_testing() {
-	TEST_ASSERT(1);
+void test_clear_screen() {
+	cpu_state.display[0] = 0xFF; // Set a pixel to white, it should be cleared out
+
+	CpuState expected_cpu_state;
+	init_state(&expected_cpu_state, NULL);
+
+	int16_t instruction = 0x0; // The instruction should not matter for this instruction clear_screen at all
+
+	clear_screen(&cpu_state, instruction);
+
+	TEST_ASSERT(state_equals(&expected_cpu_state, &cpu_state));
 }
+
 
 int main() {
 	UNITY_BEGIN();
 
-	RUN_TEST(test_testing);
+	RUN_TEST(test_clear_screen);
 
 	return UNITY_END();
 }
